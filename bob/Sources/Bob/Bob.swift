1a663a4e09dedbc4e27b7431d681bd9948de9d6c
Gaurav Gupta <gauravgupta@mobilityquarks.com>
2019-12-10 02:28:04 +0530

    finally passes all the tests
    [3/3] Linking BobPackageTests
    Test Suite 'All tests' started at 2019-12-10 02:27:40.743
    Test Suite 'BobPackageTests.xctest' started at 2019-12-10 02:27:40.743
    Test Suite 'BobTests' started at 2019-12-10 02:27:40.743
    Test Case '-[BobTests.BobTests testAskingAQustion]' started.
    Test Case '-[BobTests.BobTests testAskingAQustion]' passed (0.113 seconds).
    Test Case '-[BobTests.BobTests testCalmlySpeakingAboutUmlauts]' started.
    Test Case '-[BobTests.BobTests testCalmlySpeakingAboutUmlauts]' passed (0.000 seconds).
    Test Case '-[BobTests.BobTests testForcefulQuestions]' started.
    Test Case '-[BobTests.BobTests testForcefulQuestions]' passed (0.000 seconds).
    Test Case '-[BobTests.BobTests testOnlyNumbers]' started.
    Test Case '-[BobTests.BobTests testOnlyNumbers]' passed (0.000 seconds).
    Test Case '-[BobTests.BobTests testPrattlingOn]' started.
    Test Case '-[BobTests.BobTests testPrattlingOn]' passed (0.000 seconds).
    Test Case '-[BobTests.BobTests testProlongedSilence]' started.
    Test Case '-[BobTests.BobTests testProlongedSilence]' passed (0.000 seconds).
    Test Case '-[BobTests.BobTests testQuestionWithOnlyNumbers]' started.
    Test Case '-[BobTests.BobTests testQuestionWithOnlyNumbers]' passed (0.000 seconds).
    Test Case '-[BobTests.BobTests testShouting]' started.
    Test Case '-[BobTests.BobTests testShouting]' passed (0.000 seconds).
    Test Case '-[BobTests.BobTests testShoutingNumbers]' started.
    Test Case '-[BobTests.BobTests testShoutingNumbers]' passed (0.000 seconds).
    Test Case '-[BobTests.BobTests testShoutingWithNoExclamationmark]' started.
    Test Case '-[BobTests.BobTests testShoutingWithNoExclamationmark]' passed (0.000 seconds).
    Test Case '-[BobTests.BobTests testShoutingWithSpecialCharacters]' started.
    Test Case '-[BobTests.BobTests testShoutingWithSpecialCharacters]' passed (0.000 seconds).
    Test Case '-[BobTests.BobTests testShoutingWithUmlautsCharacters]' started.
    Test Case '-[BobTests.BobTests testShoutingWithUmlautsCharacters]' passed (0.000 seconds).
    Test Case '-[BobTests.BobTests testSilence]' started.
    Test Case '-[BobTests.BobTests testSilence]' passed (0.000 seconds).
    Test Case '-[BobTests.BobTests testStatementContainingQuestionsMark]' started.
    Test Case '-[BobTests.BobTests testStatementContainingQuestionsMark]' passed (0.000 seconds).
    Test Case '-[BobTests.BobTests testStatingSomething]' started.
    Test Case '-[BobTests.BobTests testStatingSomething]' passed (0.000 seconds).
    Test Case '-[BobTests.BobTests testTalkingForcefully]' started.
    Test Case '-[BobTests.BobTests testTalkingForcefully]' passed (0.000 seconds).
    Test Case '-[BobTests.BobTests testUsingAcronyms]' started.
    Test Case '-[BobTests.BobTests testUsingAcronyms]' passed (0.000 seconds).
    Test Suite 'BobTests' passed at 2019-12-10 02:27:40.859.
    	 Executed 17 tests, with 0 failures (0 unexpected) in 0.116 (0.116) seconds
    Test Suite 'BobPackageTests.xctest' passed at 2019-12-10 02:27:40.860.
    	 Executed 17 tests, with 0 failures (0 unexpected) in 0.116 (0.117) seconds
    Test Suite 'All tests' passed at 2019-12-10 02:27:40.860.
    	 Executed 17 tests, with 0 failures (0 unexpected) in 0.116 (0.117) seconds

--- bob/Sources/Bob/Bob.swift
+++ bob/Sources/Bob/Bob.swift
@@ -1,10 +1,10 @@
 //Solution goes in Sources
 import Foundation
 
-let questionKeyWordsSet = Set<String>(["what","where","when","how","who","Did","Does","was"])
+//let questionKeyWordsSet = Set<String>(["what","where","when","how","who","Did","Does","was"])
+//let fullStop = "."
 let questionMark = "?"
 let exclamation = "!"
-let fullStop = "."
 
 enum Answers:String {
     case sure = "Sure."
@@ -14,16 +14,16 @@ enum Answers:String {
 }
 
 func hey(_ inputStrin:String) -> String {
-
-//    let onlyNumbers = CharacterSet.alphanumerics.subtracting(CharacterSet.letters)
-//    let containsOnlyNumbers = !CharacterSet(charactersIn: inputStrin).subtracting(CharacterSet.punctuationCharacters.union(.whitespaces)).intersection(onlyNumbers).isEmpty
-
-    if inputStrin.isEmpty || (inputStrin.split(separator: Character(" ")).isEmpty){
+    let inputStrinCharSet = CharacterSet(charactersIn: inputStrin)
+    let containsOnlyNumbers = inputStrinCharSet.subtracting(CharacterSet.decimalDigits.union(.punctuationCharacters).union(.whitespacesAndNewlines)).isEmpty
+    let containsOnlyWhiteSpaces = inputStrinCharSet.subtracting(CharacterSet.whitespacesAndNewlines).isEmpty
+    if inputStrin.isEmpty || containsOnlyWhiteSpaces {
         return Answers.Fine.rawValue
-    } else if (inputStrin == inputStrin.uppercased()){// && !containsOnlyNumbers) {
-        return Answers.chillout.rawValue
-    } else if (inputStrin.hasSuffix(questionMark)){
+    } else if containsOnlyNumbers && inputStrin.hasSuffix(questionMark) || inputStrin.hasSuffix(questionMark) && !(inputStrin == inputStrin.uppercased()) {
         return Answers.sure.rawValue
+    }
+    else if inputStrin == inputStrin.uppercased() && !containsOnlyNumbers {// && !containsOnlyNumbers) {
+        return Answers.chillout.rawValue
     } else {
         return Answers.whatever.rawValue
     }
