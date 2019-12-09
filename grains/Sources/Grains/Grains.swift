1a5906fce33ed613d63936d636c4be69805308e2
Gaurav Gupta <gauravgupta@mobilityquarks.com>
2019-12-10 02:53:48 +0530

    solution which passes

--- grains/Sources/Grains/Grains.swift
+++ grains/Sources/Grains/Grains.swift
@@ -1,1 +1,23 @@
 //Solution goes in Sources
+import Foundation
+
+enum GrainsError:Error {
+    case inputTooLow(String)
+    case inputTooHigh(String)
+}
+
+func square(_ squarePos:Int) throws -> UInt {
+    switch squarePos {
+    case ...0:
+        throw
+            GrainsError.inputTooLow("Input[\(squarePos)] invalid. Input should be between 1 and 64 (inclusive)")
+    case 65...:
+        throw GrainsError.inputTooHigh("Input[\(squarePos)] invalid. Input should be between 1 and 64 (inclusive)")
+    default:
+        return UInt(pow(2.0, Double(squarePos - 1)))
+    }
+}
+
+var total:UInt {
+    return UInt.max
+}
