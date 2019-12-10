ba5ec03876423b8c30c2bd87efea8909f728288b
Gaurav Gupta <gauravgupta@mobilityquarks.com>
2019-12-10 06:04:44 +0530

    iteration 4 …with dictionary

--- raindrops/Sources/Raindrops/Raindrops.swift
+++ raindrops/Sources/Raindrops/Raindrops.swift
@@ -10,25 +10,17 @@
 let pling = "Pling"
 let plang = "Plang"
 let plong = "Plong"
-
+let soundsDict = [3:pling, 5:plang,7:plong]
 struct Raindrops {
     let num:Int
 
     var sounds:String {
         var str = ""
-        (1...num).forEach {
-            if num % $0 == 0{
-                switch $0{
-                case 3:
-                    str += pling
-                case 5:
-                    str += plang
-                case 7:
-                    str += plong
-                default:
-                    break
-                }
+        str = (1...num).reduce("") { (result, next) -> String in
+            if num % next == 0 {
+                str += soundsDict[next] ?? ""
             }
+            return str
         }
         return str.isEmpty ? "\(num)" : str
     }
@@ -37,3 +29,24 @@ struct Raindrops {
         self.num = num
     }
 }
+
+
+/**
+
+ class Raindrops {
+     let sounds: String
+
+     private static let harmonics = [
+         (3, "Pling"),
+         (5, "Plang"),
+         (7, "Plong")
+     ]
+
+     init(_ n: Int) {
+         let str = Raindrops
+             .harmonics
+             .reduce("") { $0 + (n % $1.0 == 0 ? $1.1 : "") }
+         sounds = str.isEmpty ? "\(n)" : str
+     }
+ }
+ */
