774cb6b3056c12015f8d8febf3d863f60fddee4f
Gaurav Gupta <gauravgupta@mobilityquarks.com>
2019-12-16 20:41:34 +0530

    iteration 1

--- grade-school/Sources/GradeSchool/GradeSchool.swift
+++ grade-school/Sources/GradeSchool/GradeSchool.swift
@@ -1,1 +1,26 @@
 //Solution goes in Sources
+public struct GradeSchool {
+    var roster = [Int:[String]]()
+
+    mutating func addStudent(_ name:String,grade:Int){
+        guard roster[grade] != nil else {
+            roster.updateValue([name], forKey: grade)
+            return
+        }
+        roster[grade]!.append(name)
+    }
+
+    func studentsInGrade(_ grade:Int) -> [String] {
+        return roster[grade] ?? []
+    }
+
+    var sortedRoster : [Int:[String]]{
+        var sortedDic = [Int:[String]]()
+
+        roster.keys.sorted(by: {$0 < $1}).forEach {
+            sortedDic.updateValue(roster[$0]?.sorted() ?? [], forKey: $0)
+
+        }
+        return sortedDic
+    }
+}
