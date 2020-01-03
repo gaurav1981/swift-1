98f06afa609816efb686d8521d155741bbff6fcc
Gaurav Gupta <systems.gaurav.gupta@gmail.com>
2020-01-03 05:50:37 +0530

    improvisations for timing part
    adding proj to repo

--- swift/word-count/Sources/WordCount/WordCount.swift
+++ swift/word-count/Sources/WordCount/WordCount.swift
@@ -4,12 +4,34 @@ import Foundation
 struct WordCount {
     let words:String
     func count() -> [String:Int] {
-        var count = [String:Int]()
-        let set = CharacterSet.whitespacesAndNewlines.union(.controlCharacters).union(.punctuationCharacters).union(.symbols)
-        count = words.lowercased().components(separatedBy: set).filter({!$0.isEmpty}).reduce(into: count) { (result, next) in
-            result[next, default: 0] += 1
+////        var count = [String:Int]()
+//        count = words.lowercased().components(separatedBy: CharacterSet.alphanumerics.inverted).reduce(into: [:]) { (result, next) in
+//            if !next.isEmpty{result[next, default: 0] += 1}
+//        }
+        return words.lowercased().components(separatedBy: CharacterSet.alphanumerics.inverted).reduce(into: [:]) { (result, next) in
+            if !next.isEmpty{result[next, default: 0] += 1}
         }
-        return count
     }
+
+    //optimized version
+    /**
+     struct WordCount {
+
+       init(words input: String) {
+         words = input.components(separatedBy: CharacterSet.alphanumerics.inverted)
+       }
+
+       private let words: [String]
+
+       func count() -> [String: Int] {
+         return words.reduce(into: [:]) { results, word in
+           if word.isEmpty { return }
+           results[word.lowercased(), default: 0] += 1
+         }
+       }
+
+     }
+
+     */
 }
 
